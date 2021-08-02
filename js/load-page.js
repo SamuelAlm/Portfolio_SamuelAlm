1ce944b2e81f123e68b973be3c0967ecd11bd9d8
Samuel Almeida <85806295+SamuelAlm@users.noreply.github.com>
2021-08-02 14:21:10 +0000

    02.08 alt8

--- js/load-page.js
+++ js/load-page.js
@@ -8,7 +8,29 @@ $(document).ready(function(){
   // Variavel local que recebe o atributo href do link
     let page = $(this).attr('href')
 
-    alert(page)
+    $('.modal-title').empty()
+    $('.modal-body').empty()
+
+    // Verificar qual conteudo eu devo carregar
+    switch(page){
     
+    //Se for para abrir a pagina sobre mim.
+    case 'about':
+    $('.modal-title').append('Quem sou eu?')
+    $('.modal-body').append('lorem')
+    $('.modal-info').modal('show')
+    break
+
+    case 'work':
+    break
+
+    case 'client':
+    break
+
+    case 'contact':
+    break
+    }
+
+
   })
 })
