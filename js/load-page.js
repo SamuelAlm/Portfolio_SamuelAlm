57b6353fe2be4da42f457e58f2f21df1b10c913c
Samuel Almeida <85806295+SamuelAlm@users.noreply.github.com>
2021-08-02 16:00:07 +0000

    02.08 endtest5

--- js/load-page.js
+++ js/load-page.js
@@ -14,17 +14,38 @@ $(document).ready(function(){
     switch(page){
     case 'about': 
       $('.modal-title').append('Sobre mim')
-      $('.modal-body').append('lorem')
+      $('.modal-body').append(`<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed necessitatibus accusantium quas iste nihil in rem nemo delectus nesciunt, eos soluta laudantium deserunt numquam, autem consequatur pariatur id nostrum dolore.</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem, ipsum, blanditiis omnis quis nam eveniet corporis laudantium recusandae natus repudiandae voluptatum! Magnam sit quo ullam quisquam nulla enim, labore soluta!</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat fuga earum obcaecati! Reiciendis, unde cupiditate nesciunt distinctio nulla molestias delectus, libero dolor ex mollitia ullam. Velit assumenda sit incidunt officiis!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, eum nam ab debitis natus error maxime sit quis repellendus cum. Soluta perferendis impedit doloribus eligendi, nulla iste numquam natus blanditiis!</p>`)
       $('#modal-info').modal('show')
     break
 
-    case 'work': alert('trabalhos')
+    case 'work': 
+      $('.modal-title').append('Trabalhos')
+      $('.modal-body').append(`<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed necessitatibus accusantium quas iste nihil in rem nemo delectus nesciunt, eos soluta laudantium deserunt numquam, autem consequatur pariatur id nostrum dolore.</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem, ipsum, blanditiis omnis quis nam eveniet corporis laudantium recusandae natus repudiandae voluptatum! Magnam sit quo ullam quisquam nulla enim, labore soluta!</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat fuga earum obcaecati! Reiciendis, unde cupiditate nesciunt distinctio nulla molestias delectus, libero dolor ex mollitia ullam. Velit assumenda sit incidunt officiis!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, eum nam ab debitis natus error maxime sit quis repellendus cum. Soluta perferendis impedit doloribus eligendi, nulla iste numquam natus blanditiis!</p>`)
+      $('#modal-info').modal('show')
     break
 
-    case 'client': alert('clientes')
+    case 'client': 
+      $('.modal-title').append('Cooperadores')
+      $('.modal-body').append(`<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed necessitatibus accusantium quas iste nihil in rem nemo delectus nesciunt, eos soluta laudantium deserunt numquam, autem consequatur pariatur id nostrum dolore.</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem, ipsum, blanditiis omnis quis nam eveniet corporis laudantium recusandae natus repudiandae voluptatum! Magnam sit quo ullam quisquam nulla enim, labore soluta!</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat fuga earum obcaecati! Reiciendis, unde cupiditate nesciunt distinctio nulla molestias delectus, libero dolor ex mollitia ullam. Velit assumenda sit incidunt officiis!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, eum nam ab debitis natus error maxime sit quis repellendus cum. Soluta perferendis impedit doloribus eligendi, nulla iste numquam natus blanditiis!</p>`)
+      $('#modal-info').modal('show')
     break
 
-    case 'contact': alert('contato')
+    case 'contact': 
+      $('.modal-title').append('Contato')
+      $('.modal-body').append(`<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed necessitatibus accusantium quas iste nihil in rem nemo delectus nesciunt, eos soluta laudantium deserunt numquam, autem consequatur pariatur id nostrum dolore.</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem, ipsum, blanditiis omnis quis nam eveniet corporis laudantium recusandae natus repudiandae voluptatum! Magnam sit quo ullam quisquam nulla enim, labore soluta!</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat fuga earum obcaecati! Reiciendis, unde cupiditate nesciunt distinctio nulla molestias delectus, libero dolor ex mollitia ullam. Velit assumenda sit incidunt officiis!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, eum nam ab debitis natus error maxime sit quis repellendus cum. Soluta perferendis impedit doloribus eligendi, nulla iste numquam natus blanditiis!</p>`)
+      $('#modal-info').modal('show')
     break
 
     default:
