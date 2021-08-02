b04cf98243fc52ee59b08e2dcb8f2d7d40f05435
Samuel Almeida <85806295+SamuelAlm@users.noreply.github.com>
2021-08-02 15:25:09 +0000

    02.08 test

--- js/load-page.js
+++ js/load-page.js
@@ -8,7 +8,37 @@ $(document).ready(function(){
   // Variavel local que recebe o atributo href do link
     let page = $(this).attr('href')
 
-    alert(page)
+    $('.modal-title').empty()
+    $('.modal-body').empty()
+
+    switch(page){
+    case 'about':
+    $('.modal-title').append('Quem somos')
+    $('.modal-body').append('sobre mim')
+    $('.modal-info').modal('show')
+    break;
+
+    case 'work':
+    $('.modal-title').append('Trabalhos')
+    $('.modal-body').append('work')
+    $('.modal-info').modal('show')
+    break;
+
+    case 'client':
+    $('.modal-title').append('Cooperadores')
+    $('.modal-body').append('amigos e clientes')
+    $('.modal-info').modal('show')
+    break;
+
+    case 'contact':
+    $('.modal-title').append('Contato')
+    $('.modal-body').append('redes sociais')
+    $('.modal-info').modal('show')
+    break;
+
+    default:
+    alert('Link invalido')
+    }
     
   })
 })
