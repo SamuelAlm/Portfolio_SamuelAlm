8f274b4032c01c349a87628b8c6a16698c936f6e
Samuel Almeida <85806295+SamuelAlm@users.noreply.github.com>
2021-08-02 14:58:44 +0000

    inicio 02.08 refazendo

--- js/load-page.js
+++ js/load-page.js
@@ -8,40 +8,8 @@ $(document).ready(function(){
   // Variavel local que recebe o atributo href do link
     let page = $(this).attr('href')
 
-    $('.modal-title').empty()
-    $('.modal-body').empty()
 
-    // Verificar qual conteudo eu devo carregar
-    switch(page){
-    
-    //Se for para abrir a pagina sobre mim.
-    case "about" :
-    $('.modal-title').append('Quem Sou')
-    $('.modal-body').append('Sobre mim')
-    $('.modal-info').modal('show')
-    break
-
-    case "work" :
-    $('.modal-title').append('Trabalhos')
-    $('.modal-body').append('meus trabalhos')
-    $('.modal-info').modal('show")
-    break
-
-    case "client" :
-    $('.modal-title').append('Cooperadores')
-    $('.modal-body').append('amigos e clientes')
-    $('.modal-info').modal('show')
-    break
-
-    case "contact" :
-    $('.modal-title').append('Contato')
-    $('.modal-body').append('redes sociais')
-    $('.modal-info').modal('show')
-    break
-
-    default: alert('Link não encontrado')
-    }
-
+    alert(page)
 
   })
 })
