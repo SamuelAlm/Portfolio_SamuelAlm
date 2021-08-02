a8bf15798c09e91629f56583df2ce0289c25da50
Samuel Almeida <85806295+SamuelAlm@users.noreply.github.com>
2021-08-02 15:42:25 +0000

    test4

--- js/load-page.js
+++ js/load-page.js
@@ -8,8 +8,14 @@ $(document).ready(function(){
    // Variavel local que recebe o atributo href do link
     let page = $(this).attr('href')
 
+    $('.modal-title').empty()
+    $('.modal-body').empty()
+
     switch(page){
-    case 'about': alert('quem sou eu?')
+    case 'about': 
+      $('.modal-title').append('Sobre mim')
+      $('.modal-body').append('lorem')
+      $('.modal-info').modal('show')
     break
 
     case 'work': alert('trabalhos')
